c8f9e26c8e8b859e0c5445cf16e24b4a8f76efce
Aditya <adityachoudhary2602@gmail.com>
2026-07-31 09:29:46 +0530

    Basic JS

---
+++ JS/demo2.js
@@ -0,0 +1,12 @@
+export function  sum(...num){
+    const sum=num.reduce((s,i)=>(s+1));
+    return sum;
+}
+export const add=(...num)=>{
+    let s=0;
+    for(let i of sum)
+    {
+        s=s+i;
+    }
+    return s;
+}
---
+++ JS/test.js
@@ -0,0 +1,7 @@
+import {sum} from "./demo2.js";
+console.log("sum=",sum(1));
+console.log("sum=",sum(54,23,6));
+console.log("sum=",sum(10,5,2));
+console.log("sum=",sum(89,54,2));
+console.log("sum=",sum(34,56,47));
+
